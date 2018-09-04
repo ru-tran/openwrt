27718a68acb394cfb79a22416d194cf167740475
Dan <dan@ru-tran.dev>
2018-09-04 14:24:29 +0300

    netifd: proto-dhcp: handle alias interfaces
    The alias interfaces for interfaces with proto dhcp are very useful for
    specialised builds with proto dhcp on lan by default but with backup static
    addresses.
    
    The default way has one shortage. It left backup address configured on
    interface with address configured by odchp. This can lead to network
    overlapping (when dhcp server configured on the same subnet with alias
    interface) and to the addresses duplication inside broadcast domain
    (when more then one board with same firmware running in subnet).
    
    This patch introduces new option to dhcp proto. Just add
    
            option alias '<alias interface(s) name(s)>'
    
    to the interface configuration and it will automatically downs alias
    interfaces you specified upon dhcp address receiving.

--- package/network/config/netifd/files/lib/netifd/proto/dhcp.sh
+++ package/network/config/netifd/files/lib/netifd/proto/dhcp.sh
@@ -25,6 +25,7 @@ proto_dhcp_init_config() {
 	proto_config_add_string mtu6rd
 	proto_config_add_string customroutes
 	proto_config_add_boolean classlessroute
+	proto_config_add_string 'alias'
 }
 
 proto_dhcp_add_sendopts() {
@@ -35,8 +36,8 @@ proto_dhcp_setup() {
 	local config="$1"
 	local iface="$2"
 
-	local ipaddr hostname clientid vendorid broadcast norelease reqopts defaultreqopts iface6rd sendopts delegate zone6rd zone mtu6rd customroutes classlessroute
-	json_get_vars ipaddr hostname clientid vendorid broadcast norelease reqopts defaultreqopts iface6rd delegate zone6rd zone mtu6rd customroutes classlessroute
+	local ipaddr hostname clientid vendorid broadcast release reqopts defaultreqopts iface6rd sendopts delegate zone6rd zone mtu6rd customroutes classlessroute alias
+	json_get_vars ipaddr hostname clientid vendorid broadcast release reqopts defaultreqopts iface6rd delegate zone6rd zone mtu6rd customroutes classlessroute alias
 
 	local opt dhcpopts
 	for opt in $reqopts; do
@@ -61,6 +62,12 @@ proto_dhcp_setup() {
 	[ "$delegate" = "0" ] && proto_export "IFACE6RD_DELEGATE=0"
 	# Request classless route option (see RFC 3442) by default
 	[ "$classlessroute" = "0" ] || append dhcpopts "-O 121"
+
+	proto_export "ALIAS=$alias"
+	local i
+	for i in $alias; do
+		ubus call network.interface."$i" up '{}'
+	done
 
 	proto_export "INTERFACE=$config"
 	proto_run_command "$config" udhcpc \
